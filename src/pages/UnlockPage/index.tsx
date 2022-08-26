be8c6c1a74abf81deb69e7098143cdaf731af8c1
Radu Mojic <radu.mojic@elrond.com>
2022-08-26 17:54:08 +0300

    [+] dapp-core-2

--- src/pages/UnlockPage/index.tsx
+++ src/pages/UnlockPage/index.tsx
@@ -7,39 +7,41 @@ import {
 } from '@elrondnetwork/dapp-core/UI';
 import { routeNames } from 'routes';
 
-export const UnlockRoute: () => JSX.Element = () => (
-  <div className='home d-flex flex-fill align-items-center'>
-    <div className='m-auto' data-testid='unlockPage'>
-      <div className='card my-4 text-center'>
-        <div className='card-body py-4 px-2 px-sm-2 mx-lg-4'>
-          <h4 className='mb-4'>Login</h4>
-          <p className='mb-4'>pick a login method</p>
+export const UnlockRoute: () => JSX.Element = () => {
+  return (
+    <div className='home d-flex flex-fill align-items-center'>
+      <div className='m-auto' data-testid='unlockPage'>
+        <div className='card my-4 text-center'>
+          <div className='card-body py-4 px-2 px-sm-2 mx-lg-4'>
+            <h4 className='mb-4'>Login</h4>
+            <p className='mb-4'>pick a login method</p>
 
-          <ExtensionLoginButton
-            callbackRoute={routeNames.dashboard}
-            loginButtonText={'Extension'}
-          />
-          <WebWalletLoginButton
-            callbackRoute={routeNames.dashboard}
-            loginButtonText={'Web wallet'}
-          />
-          <LedgerLoginButton
-            loginButtonText={'Ledger'}
-            callbackRoute={routeNames.dashboard}
-          />
-          <WalletConnectLoginButton
-            callbackRoute={routeNames.dashboard}
-            loginButtonText={'Maiar'}
-          />
-          <WalletConnectLoginButton
-            callbackRoute={routeNames.dashboard}
-            loginButtonText={'Maiar V2'}
-            isWalletConnectV2={true}
-          />
+            <ExtensionLoginButton
+              callbackRoute={routeNames.dashboard}
+              loginButtonText={'Extension'}
+            />
+            <WebWalletLoginButton
+              callbackRoute={routeNames.dashboard}
+              loginButtonText={'Web wallet'}
+            />
+            <LedgerLoginButton
+              loginButtonText={'Ledger'}
+              callbackRoute={routeNames.dashboard}
+            />
+            <WalletConnectLoginButton
+              callbackRoute={routeNames.dashboard}
+              loginButtonText={'Maiar'}
+            />
+            <WalletConnectLoginButton
+              callbackRoute={routeNames.dashboard}
+              loginButtonText={'Maiar V2'}
+              isWalletConnectV2={true}
+            />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default UnlockRoute;
