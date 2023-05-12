2d5a33c32bbaa8d2ed4e8f16f2de25086bc5cf91
Radu Mojic <radu.mojic@elrond.com>
2023-05-12 15:23:47 +0300

    [+] updated pakages

--- src/pages/Dashboard/components/Actions/helpers/smartContract.ts
+++ src/pages/Dashboard/components/Actions/helpers/smartContract.ts
@@ -1,17 +1,11 @@
-import {
-  AbiRegistry,
-  SmartContractAbi,
-  SmartContract,
-  Address
-} from '@multiversx/sdk-core/out';
+import { AbiRegistry, SmartContract, Address } from '@multiversx/sdk-core/out';
 
 import { contractAddress } from 'config';
 import json from 'ping-pong.abi.json';
 
 const abiRegistry = AbiRegistry.create(json);
-const abi = new SmartContractAbi(abiRegistry);
 
 export const smartContract = new SmartContract({
   address: new Address(contractAddress),
-  abi
+  abi: abiRegistry
 });
